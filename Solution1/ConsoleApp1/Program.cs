89c02c79891850bafa6df3d1b24ba198627b12ef
Kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-14 19:57:57 +0100

    Zadanie3

--- Solution1/ConsoleApp1/Program.cs
+++ Solution1/ConsoleApp1/Program.cs
@@ -5,3 +5,17 @@ Console.WriteLine("Change 1");
 Console.WriteLine("Change 2");
 Console.WriteLine("Change 3");
 
+int[] arr = { 1, 2, 3, 4 };
+
+Console.WriteLine(AverageCalculator(arr));
+
+static double AverageCalculator(int[] numbers)
+{
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
